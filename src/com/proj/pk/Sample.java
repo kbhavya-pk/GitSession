4eb562cb0a817298f555733f6ed457845269ff50
kbhavya-pk <kbhavya@prokarma.com>
2018-03-16 15:25:31 +0530

    Issue

--- src/com/proj/pk/Sample.java
+++ src/com/proj/pk/Sample.java
@@ -5,7 +5,7 @@ public class Sample {
 	public static void main(String[] args) {
 		System.out.println("Bhavya");
 		System.out.println("Raghavi");
-		System.out.println("Anbu");
+		System.out.println("Karan");
 	}
 
 }
